4d04162538c3cb0cb1ea6eb23b1d891ee35d070b
Adeloba Joshua <adelobajoshua19@gmail.com>
2023-09-28 15:07:42 +0100

    baseUrl added

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import {toast} from 'react-toastify'
 
 export const api = axios.create({
-    baseURL: "https://homyz-steel.vercel.app/api"
+    baseURL: "https://homyz-gamma.vercel.app/api"
 })
 
 export const getAllProperties = async() => {
